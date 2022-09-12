d111e94f8b643db5e39823a60fee2c621454df89
JinkwonHeo <hjinkwon912@gmail.com>
2022-09-13 01:17:59 +0900

    Feat/7: 문제풀이 페이지 css 적용

--- src/components/share/Button.tsx
+++ src/components/share/Button.tsx
@@ -1,17 +1,17 @@
 import styled from 'styled-components';
 
 export const Button = styled.button`
-  width: fit-content;
-  padding: 0.3rem;
-  margin-top: 0.5rem;
-  background-color: ${({ theme }) => theme.colors.white};
+  width: 80%;
+  padding: 1rem 0;
+  margin: 0.5rem 0;
   border: none;
   border-radius: 15px;
+  background-color: ${({ theme }) => theme.colors.white};
   font-size: 1rem;
   font-family: 'Pretendard-medium';
   transition-duration: 0.4s;
-  box-shadow: 5px 5px 8px 3px rgb(0 0 0 / 5%), 2px 2px 5px -2px rgba(0, 0, 0, 0.118),
-    5px 2px 5px -7px rgb(0 0 0 / 5%);
+  box-shadow: 5px 5px 8px 3px rgb(0 0 0 / 20%), 2px 2px 5px -2px rgba(0, 0, 0, 0.218),
+    5px 2px 5px -7px rgb(0 0 0 / 20%);
   &:hover {
     background-color: ${({ theme }) => theme.colors.gray};
     color: ${({ theme }) => theme.colors.white};
