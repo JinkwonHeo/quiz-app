445dd4845877215ea0c0dc8446b0a9b372f4e3ce
JinkwonHeo <hjinkwon912@gmail.com>
2022-09-12 23:57:18 +0900

    Feat/6: 폰트 적용, 메인화면 css 적용

--- src/components/share/Button.tsx
+++ src/components/share/Button.tsx
@@ -4,12 +4,14 @@ export const Button = styled.button`
   width: fit-content;
   padding: 0.3rem;
   margin-top: 0.5rem;
-  background-color: transparent;
-  border-width: 1px;
-  border-radius: 3px;
-  border-color: #e9e9e9;
+  background-color: ${({ theme }) => theme.colors.white};
+  border: none;
+  border-radius: 15px;
   font-size: 1rem;
+  font-family: 'Pretendard-medium';
   transition-duration: 0.4s;
+  box-shadow: 5px 5px 8px 3px rgb(0 0 0 / 5%), 2px 2px 5px -2px rgba(0, 0, 0, 0.118),
+    5px 2px 5px -7px rgb(0 0 0 / 5%);
   &:hover {
     background-color: ${({ theme }) => theme.colors.gray};
     color: ${({ theme }) => theme.colors.white};
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
 import { Button } from '../components/share/Button';
+import { Container } from '../components/share/Container';
+import { FlexContainer } from '../components/share/FlexContainer';
 
 export default function MainPage() {
   const navigate = useNavigate();
@@ -15,9 +18,29 @@ export default function MainPage() {
 
   return (
     <>
-      <div>여기에 이미지 넣을 것</div>
-      <Button onClick={handleStartQuizButtonClick}>문제 풀기</Button>
-      <Button onClick={handleMoveReviewPageButton}>오답 노트</Button>
+      <Container>
+        <FlexContainer>
+          <Title>Quiz!</Title>
+          <MainPageButton onClick={handleStartQuizButtonClick}>문제 풀기</MainPageButton>
+          <MainPageButton onClick={handleMoveReviewPageButton}>오답 노트</MainPageButton>
+        </FlexContainer>
+      </Container>
     </>
   );
 }
+
+const Title = styled.div`
+  margin-bottom: 5rem;
+  font-family: 'Pretendard-bold';
+  font-size: 4rem;
+  color: white;
+  text-shadow: 6px 6px 12px black;
+`;
+
+const MainPageButton = styled(Button)`
+  width: 80%;
+  padding: 1rem 0;
+  margin: 0.5rem 0;
+  box-shadow: 5px 5px 8px 3px rgb(0 0 0 / 20%), 2px 2px 5px -2px rgba(0, 0, 0, 0.218),
+    5px 2px 5px -7px rgb(0 0 0 / 20%);
+`;
